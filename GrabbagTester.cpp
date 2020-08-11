42f86f5aec0d77e8bd48402a4804dce2e5fb2298
Keith Schwarz <htiek@cs.stanford.edu>
2020-08-10 20:29:49 -0700

    Cache loaded Grabbag to avoid reprocessing COBS each time.

--- GrabbagTester.cpp
+++ GrabbagTester.cpp
@@ -5,11 +5,18 @@
 #include <iostream>
 using namespace std;
 
+namespace {
+    /* Loads the given Grabbag file. */
+    Grabbag loadGrabbag() {
+        ifstream input("res/tests/assignment.grabbag", ios::binary);
+        return Grabbag(input);
+    }
+}
+
 void runPrivateTest(const string& testName,
                     function<void(istream&)> callback) {
-    /* Load the grabbag. */
-    ifstream input("res/tests/assignment.grabbag", ios::binary);
-    Grabbag grabbag(input);
+    /* Cache for future use. */
+    static Grabbag grabbag = loadGrabbag();
 
     istringstream source(grabbag.contentsOf(testName));
     callback(source);
